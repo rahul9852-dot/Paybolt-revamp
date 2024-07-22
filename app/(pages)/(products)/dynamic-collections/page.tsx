2fd6f282536d4ed107472acd99d13cd26dee46d6
Rahul <srayesh5432@gmail.com>
2024-07-22 17:36:00 +0530

    remove santry

--- app/(pages)/(products)/dynamic-collections/page.tsx
+++ app/(pages)/(products)/dynamic-collections/page.tsx
@@ -1,4 +1,4 @@
-
+"use client"
 import MagicButton from "@/components/MagicButton";
 import { CanvasRevealEffect } from "@/components/ui/CanvasRevealEffect";
 import { Button } from "@/components/ui/MovingBorders";
